32a09503126ce46ea534c7dbe5187185f08bd0c5
Daniel Bolton <dbb@9y.com>
2011-08-21 21:58:20 -0400

    Messing around with test.js

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
-#!/usr/bin/env smjs
+#!/usr/bin/env js
 
-// spidermonkey-bin
+// spidermonkey-bin version 7.0~b1-3 (amd64) from Debian exp
+// /usr/bin/X11/js -> /etc/alternatives/js -> /usr/bin/smjs
 
 print( "A real JavaScript script. How cool is that?" );
 
